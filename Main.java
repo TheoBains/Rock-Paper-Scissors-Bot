464449568c083f025e8c64d727bf9887eb0c2050
Theo <67968514+TheoBains@users.noreply.github.com>
2024-06-14 21:58:17 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -13,16 +13,16 @@ class Main {
       String choice = keyboard.nextLine();
 
       if (choice.equals("scissors") || choice.equals("Scissors")) {
-        System.out.println("\nAwh, you lose. The computer chose rock. Better luck next time :(\n\n");
+        System.out.println("\nYou lose. The computer chose rock. Better luck next time :(\n\n");
       }
       else if (choice.equals("paper") || choice.equals("Paper")) {
         System.out.println("\nThe computer chose scissors. Don't be discouraged, next time is the one! You've got it!\n\n");
       }
       else if (choice.equals("rock") || choice.equals("Rock")) {
-        System.out.println("\nI have some truly sad news, the computer chose paper. I know this news may be hard to take but I'm sure you can make it through. I'm here for support. You'll probably win next time.\n\n");
+        System.out.println("\nThe computer chose paper. I know this news may be hard to take but I'm sure you can make it through. I'm here for support. You'll probably win next time.\n\n");
       }
       else if (choice.equals("I can't handle the heat")) {
-      System.out.println("\nYou're a weak-minded loser who can't even win a game based off of luck. Practice more. Dweeb.");
+      System.out.println("\nDweeb.");
       System.exit(0);
       }
       else {
